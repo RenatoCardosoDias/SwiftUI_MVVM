2988200fcd63da7eea8b13a5ec74c97ee71aac74
Renato Dias <renatocardosodias@icloud.com>
2023-02-03 17:41:12 -0300

    Initial Commit

---
+++ MVVM_SWIFTUI/MVVM_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SWIFTUIApp.swift
+//  MVVM_SWIFTUI
+//
+//  Created by Renato on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
